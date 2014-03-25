634fe89eacab4dc30f5cd9b4e1167fe40d099dad
Lopno <mikaelmidt@gmail.com>
2014-03-25 22:17:24 +0100

    api for registering player

--- index.php
+++ index.php
@@ -2,20 +2,22 @@
 require 'flight/Flight.php';
 require_once 'DB.php';
 
+date_default_timezone_set("Europe/Copenhagen");
+
 function match($location, $teama, $teamb, $scorea, $scoreb){
     global $DB;
-    $draw = FALSE;
-    $aiswinner = FALSE;
-    echo "inserts match<br/>";
-    echo "$teama $scorea - $scoreb $teamb<br/>";
     $result = $DB->query("INSERT INTO matches 
-                              (location,teamA,teamB,teamAScore,teamBScore) VALUES 
-                              ($location,$teama,$teamb,$scorea,$scoreb)");
+                          (location,teamA,teamB,teamAScore,teamBScore) VALUES 
+                          ($location,$teama,$teamb,$scorea,$scoreb)");
 }
 
 function register($rfid, $name){
+    global $DB;
     echo "registers player<br/>";
     echo "Player with RFID $rfid registered as $name<br/>";
+    $result = $DB->query("INSERT INTO players
+                          (rfid,name) VALUES
+                          ('$rfid','$name')");
 }
 
 Flight::route('/match/@location/@teama/@teamb/@scorea/@scoreb', 'match');
