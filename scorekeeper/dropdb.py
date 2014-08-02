8e5e2e6c05fd0293aa8402b49e4fdc43c3440c53
alex <abondoa@gmail.com>
2014-08-03 00:35:57 +0200

    scorekeeper is running, it is alive

--- scorekeeper/dropdb.py
+++ scorekeeper/dropdb.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python2
+import sys
+import os
+sys.path.insert(1,os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from models.model import dropSchema
 
 
--- scorekeeper/initdb.py
+++ scorekeeper/initdb.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python2
 from red.config      import config, initConfig
+import sys
+import os
+sys.path.insert(1,os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 configFile='meta.conf'
 initConfig(configFile)
